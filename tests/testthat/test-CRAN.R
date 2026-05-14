264c82f5fc1f6ed17f90f59a031a95af2f6ba5c4
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-05-14 16:06:26 -0400

    suggests

--- tests/testthat/test-CRAN.R
+++ tests/testthat/test-CRAN.R
@@ -1,1 +1,11 @@
 library(testthat)
+
+test_that("generate_data works", {
+  N_list <- Poncatime:::generate_data_interface(10, 100, 10)
+  expect_equal(length(N_list$pointVec), 60)
+  expect_equal(length(N_list$queriesVec), 300)
+  count <- with(N_list, Poncatime:::asoCurvatureEstimation_interface(pointVec, queriesVec, 10))
+})
+
+
+
