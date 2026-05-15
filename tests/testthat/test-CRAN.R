9ceda43ec0957f906f87079698caff83e05a6479
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-05-15 14:27:18 -0400

    first atime figure

--- tests/testthat/test-CRAN.R
+++ tests/testthat/test-CRAN.R
@@ -1,15 +1,12 @@
 library(testthat)
 
-
-
 test_that("generate_data works", {
-  N_data <- 10000
+  N_points <- 10000
   N_queries <- 1000
   dataScale <- 10
-  points <- matrix(nrow=N_data, ncol=6);
-  queries <- matrix(nrow=N_queries, ncol=3);
-
-
-  Poncatime:::generate_data_interface(points, queries, dataScale)
+  N_list <- Poncatime:::generate_data_interface(N_points, N_queries, dataScale)
+  expect_equal(nrow(N_list$queries), N_queries)
+  expect_equal(nrow(N_list$points), N_points)
   (count <- with(N_list, Poncatime:::asoCurvatureEstimation_interface(points, queries, dataScale/5)))
+  expect_equal(count, N_queries)
 })
--- tests/testthat/test-small.R
+++ tests/testthat/test-small.R
@@ -1,4 +1,4 @@
-N_data <- 10000
-N_queries <- 1000
+N_data <- 100
+N_queries <- 10
 N_list <- Poncatime:::generate_data_interface(N_data, N_queries, 10)
 count <- with(N_list, Poncatime:::asoCurvatureEstimation_interface(points, queries, 10))
