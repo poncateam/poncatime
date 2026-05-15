afe6f986836f3f37e743bda93a8d0746587aad1a
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-05-15 17:01:58 -0400

    Np=Nq

--- .ci/atime/tests.R
+++ .ci/atime/tests.R
@@ -2,9 +2,20 @@ remove.units <- "kilobytes"
 base.ref <- "main"
 test.list <- atime::atime_test_list(
   seconds.limit=0.1,
-  asoCurvatureEstimation=atime::atime_test(
+  "asoCurvatureEstimation(Np=Nq*10)"=atime::atime_test(
     setup={
       N_points <- N*10
+      N_queries <- N
+      dataScale <- 10
+      N_list <- Poncatime:::generate_data_interface(N_points, N_queries, dataScale)
+    },
+    result=TRUE,
+    first_valgrind_ok="c9c2aa49c23f7b06ab06fa67e5f4066856c801f4", # first version which compiles and runs without valgrind read errors.
+    expr=with(N_list, data.frame(count=Poncatime:::asoCurvatureEstimation_interface(points, queries, dataScale/5)))
+  ),
+  "asoCurvatureEstimation(Np=Nq)"=atime::atime_test(
+    setup={
+      N_points <- N
       N_queries <- N
       dataScale <- 10
       N_list <- Poncatime:::generate_data_interface(N_points, N_queries, dataScale)
