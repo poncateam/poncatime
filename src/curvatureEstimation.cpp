2e09430a25b95454a85a2ec09dabaaa497ac3474
Nicolas Mellado <nmellado0@gmail.com>
2026-05-25 13:41:56 -0400

    Update Ponca to v2.alpha2

--- src/curvatureEstimation.cpp
+++ src/curvatureEstimation.cpp
@@ -173,7 +173,10 @@ ComputeReturnType computeFit(const Eigen::MatrixXd& queries, Param p)
 
 using NF        = DistWeightFunc<MyPointSimple, SmoothWeightKernel<double> > ;
 using ASOBasket = Basket<MyPointSimple, NF, OrientedSphereFit>;
-using ASOFit    = BasketDiff<ASOBasket, FitSpaceDer, OrientedSphereDer, MlsSphereFitDer>;
+using ASOFit    = BasketDiff<ASOBasket, FitSpaceDer, OrientedSphereDer, MlsSphereFitDer,
+                    CurvatureEstimatorDer,
+                    NormalDerivativeWeingartenEstimator,WeingartenCurvatureEstimatorDer>;
+
 using PlaneFit  = Ponca::Basket<MyPointSimple, NF, CovariancePlaneFit>;
 
 int asoCurvatureEstimation(const Eigen::MatrixXd& queries, double scale, int& meanNeiSize)
