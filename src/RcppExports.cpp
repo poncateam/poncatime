f7af5d105053f697d8ddbe2343718fea758e05b2
Nicolas Mellado <nmellado0@gmail.com>
2026-05-29 13:33:56 -0400

    Update V0X3 interface and tests according to master (PR#19)

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -36,7 +36,7 @@ BEGIN_RCPP
 END_RCPP
 }
 // asoCurvatureEstimation_interface
-int asoCurvatureEstimation_interface(const Eigen::MatrixXd& queries, const double scale);
+Rcpp::DataFrame asoCurvatureEstimation_interface(const Eigen::MatrixXd& queries, const double scale);
 RcppExport SEXP _Poncatime_asoCurvatureEstimation_interface(SEXP queriesSEXP, SEXP scaleSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -47,8 +47,20 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// asoCurvatureEstimationK_interface
+Rcpp::DataFrame asoCurvatureEstimationK_interface(const Eigen::MatrixXd& queries, const int k);
+RcppExport SEXP _Poncatime_asoCurvatureEstimationK_interface(SEXP queriesSEXP, SEXP kSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type queries(queriesSEXP);
+    Rcpp::traits::input_parameter< const int >::type k(kSEXP);
+    rcpp_result_gen = Rcpp::wrap(asoCurvatureEstimationK_interface(queries, k));
+    return rcpp_result_gen;
+END_RCPP
+}
 // planeFit_interface
-int planeFit_interface(const Eigen::MatrixXd& queries, const double scale);
+Rcpp::DataFrame planeFit_interface(const Eigen::MatrixXd& queries, const double scale);
 RcppExport SEXP _Poncatime_planeFit_interface(SEXP queriesSEXP, SEXP scaleSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -59,12 +71,26 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// planeFitK_interface
+Rcpp::DataFrame planeFitK_interface(const Eigen::MatrixXd& queries, const int k);
+RcppExport SEXP _Poncatime_planeFitK_interface(SEXP queriesSEXP, SEXP kSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Eigen::MatrixXd& >::type queries(queriesSEXP);
+    Rcpp::traits::input_parameter< const int >::type k(kSEXP);
+    rcpp_result_gen = Rcpp::wrap(planeFitK_interface(queries, k));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_Poncatime_generatePointClouds_interface", (DL_FUNC) &_Poncatime_generatePointClouds_interface, 3},
     {"_Poncatime_buildKdTree_interface", (DL_FUNC) &_Poncatime_buildKdTree_interface, 1},
     {"_Poncatime_asoCurvatureEstimation_interface", (DL_FUNC) &_Poncatime_asoCurvatureEstimation_interface, 2},
+    {"_Poncatime_asoCurvatureEstimationK_interface", (DL_FUNC) &_Poncatime_asoCurvatureEstimationK_interface, 2},
     {"_Poncatime_planeFit_interface", (DL_FUNC) &_Poncatime_planeFit_interface, 2},
+    {"_Poncatime_planeFitK_interface", (DL_FUNC) &_Poncatime_planeFitK_interface, 2},
     {NULL, NULL, 0}
 };
 
